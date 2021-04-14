1c6b41944a3e78d130c9aabf2a38419531579ef4
Gyor <zoltan.laszlo.gyor@gmail.com>
2021-04-14 17:04:33 +0100

    Did the 2nd exercise

--- Exercises.java
+++ Exercises.java
@@ -1,20 +1,38 @@
 import java.util.TreeMap;
 
 public class Exercises {
-   TreeMap<String, Integer> treeMap = new TreeMap<String, Integer>();
+    TreeMap<String, Integer> treeMap = new TreeMap<String, Integer>();
 
-   public void exercise1(TreeMap<String, Integer> treeMap)
-   {
-       System.out.println("1st exercise: Write a Java program to associate the specified value with the specified key in a Tree Map");
-       treeMap.put("Fjord", 1);
-       treeMap.put("Jester", 2);
-       treeMap.put("Caleb", 3);
-       System.out.println(treeMap);
-       System.out.println();
-   }
+    public void exercise1(TreeMap<String, Integer> treeMap, int selector)
+    {
+        System.out.println("1st exercise: Write a Java program to associate the specified value with the specified key in a Tree Map");
+        if(selector == 1){
+            treeMap.put("Fjord", 1);
+            treeMap.put("Jester", 2);
+            treeMap.put("Caleb", 3);
+        }else if(selector ==  2){
+            treeMap.put("Veth", 4);
+            treeMap.put("Yahsa", 5);
+            treeMap.put("Caduceus", 6);
+            treeMap.put("Beauregard", 7);
+        }
+       
+        System.out.println(treeMap);
+        System.out.println();
+    }
 
-   public TreeMap<String, Integer> getTreeMap()
-   {
-       return treeMap;
-   }
+    public void exercise2(TreeMap<String, Integer> treeMap1, TreeMap<String, Integer> treeMap2)
+    {
+        System.out.println("2nd exercise: Write a Java program to copy a Tree Map content to another Tree Map");
+        System.out.println("1st map: " + treeMap1);
+        System.out.println("2nd map: " + treeMap2);
+        treeMap2 = (TreeMap<String, Integer>) treeMap1.clone();
+        System.out.println("2nd map after copying: " + treeMap2);
+        System.out.println();
+    }
+
+    public TreeMap<String, Integer> getTreeMap()
+    {
+        return treeMap;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Exercises table1 = new Exercises();
+        Exercises table2 = new Exercises();
 
-        table1.exercise1(table1.getTreeMap());
+        table1.exercise1(table1.getTreeMap(), 1);
+        table2.exercise1(table2.getTreeMap(), 2);
+
+        table1.exercise2(table1.getTreeMap(), table2.getTreeMap());
     }
 }
