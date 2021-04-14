f043b368518615b15978c853446e4e504763d8e7
Gyor <zoltan.laszlo.gyor@gmail.com>
2021-04-14 16:52:14 +0100

    Did the 1st exercise and created the getter function

--- Exercises.java
+++ Exercises.java
@@ -1,3 +1,20 @@
+import java.util.TreeMap;
+
 public class Exercises {
-    
+   TreeMap<String, Integer> treeMap = new TreeMap<String, Integer>();
+
+   public void exercise1(TreeMap<String, Integer> treeMap)
+   {
+       System.out.println("1st exercise: Write a Java program to associate the specified value with the specified key in a Tree Map");
+       treeMap.put("Fjord", 1);
+       treeMap.put("Jester", 2);
+       treeMap.put("Caleb", 3);
+       System.out.println(treeMap);
+       System.out.println();
+   }
+
+   public TreeMap<String, Integer> getTreeMap()
+   {
+       return treeMap;
+   }
 }
--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        Exercises table1 = new Exercises();
+
+        table1.exercise1(table1.getTreeMap());
+    }
 }
